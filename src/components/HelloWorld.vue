b7a2924400e937583ea0c958fa01df2719484ce6
Sara Rooijakkers <sara.rooijakkers@gmail.com>
2021-06-23 14:36:59 +0200

    Added tailwind.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,70 +1,75 @@
 <template>
-  <h1>{{ msg }}</h1>
+    <h1>{{ msg }}</h1>
 
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a
-      href="https://marketplace.visualstudio.com/items?itemName=octref.vetur"
-      target="_blank"
-    >
-      Vetur
-    </a>
-    or
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    (if using
-    <code>&lt;script setup&gt;</code>)
-  </p>
+    <p>
+        Recommended IDE setup:
+        <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+        +
+        <a
+            href="https://marketplace.visualstudio.com/items?itemName=octref.vetur"
+            target="_blank"
+        >
+            Vetur
+        </a>
+        or
+        <a href="https://github.com/johnsoncodehk/volar" target="_blank"
+            >Volar</a
+        >
+        (if using
+        <code>&lt;script setup&gt;</code>)
+    </p>
 
-  <p>See <code>README.md</code> for more information.</p>
+    <p>See <code>README.md</code> for more information.</p>
 
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
+    <p>
+        <a href="https://vitejs.dev/guide/features.html" target="_blank">
+            Vite Docs
+        </a>
+        |
+        <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
+    </p>
 
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+    <button type="button" @click="count++">count is: {{ count }}</button>
+    <p>
+        Edit
+        <code>components/HelloWorld.vue</code> to test hot module replacement.
+    </p>
 </template>
 
 <script lang="ts">
-import { ref, defineComponent } from 'vue'
+import { ref, defineComponent } from "vue";
 export default defineComponent({
-  name: 'HelloWorld',
-  props: {
-    msg: {
-      type: String,
-      required: true
-    }
-  },
-  setup: () => {
-    const count = ref(0)
-    return { count }
-  }
-})
+    name: "HelloWorld",
+    props: {
+        msg: {
+            type: String,
+            required: true,
+        },
+    },
+    setup: () => {
+        const count = ref(0);
+        return { count };
+    },
+});
 </script>
 
 <style scoped>
 a {
-  color: #42b983;
+    @apply text-green-500;
 }
 
 label {
-  margin: 0 0.5em;
-  font-weight: bold;
+    @apply mx-2 font-bold;
 }
 
 code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
+    @apply bg-gray-100;
+    @apply px-2 py-1 rounded;
+    @apply text-gray-700;
+}
+
+button {
+    @apply my-4 px-2 py-1 rounded;
+    @apply bg-blue-400;
 }
 </style>
