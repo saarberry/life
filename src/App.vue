d3b039f06bff7525dc7b0ad9be4accc5b3f5357c
Sara Rooijakkers <sara.rooijakkers@gmail.com>
2021-06-29 21:27:29 +0200

    Fixed evolution logic. Allow drawing of shapes to start the game.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
-    <div class="Row" v-for="(row, i) in cellGrid" :key="`row-${i}`">
+    <div class="Row" v-for="(row, y) in cellGrid" :key="`row-${y}`">
         <Cell
-            v-for="(alive, j) in row"
-            :key="`row-${i}-col-${j}`"
+            v-for="(alive, x) in row"
+            :key="`row-${y}-col-${x}`"
             :alive="alive"
+            @click="onClick(x, y)"
         />
     </div>
+
+    <button class="Start" @click="start">Start</button>
 </template>
 
 <script lang="ts">
@@ -25,62 +28,74 @@ export default defineComponent({
 
         // Make just enough cells to cover the screen.
         function generateCellsForWindow() {
-            const cols = Math.floor(window.innerWidth / cellSize);
-            const rows = Math.floor(window.innerHeight / cellSize);
-
-            console.log(cols, rows);
+            const colCount = Math.floor(window.innerWidth / cellSize);
+            const rowCount = Math.floor(window.innerHeight / cellSize);
 
             // Clear the existing cell map.
-            cellGrid.value = [];
+            let rows: boolean[][] = [];
 
             // Fill it with new cells.
-            for (let currentRow = 0; currentRow < rows; currentRow++) {
-                const row: boolean[] = [];
-                for (let currentCol = 0; currentCol < cols; currentCol++) {
-                    const alive = Math.random() > 0.6;
-                    row.push(alive);
+            for (let currentRow = 0; currentRow < rowCount; currentRow++) {
+                let row: boolean[] = [];
+                for (let currentCol = 0; currentCol < colCount; currentCol++) {
+                    const alive = Math.random() > 0.9;
+                    row.push(false);
                 }
 
-                cellGrid.value.push(row);
+                rows.push(row);
             }
+
+            cellGrid.value = rows;
         }
 
         // Generate initial cell state.
         generateCellsForWindow();
 
         // Loop at a set interval to play the game.
-        const loop = setInterval(() => {
-            // Copy the cell grid as a reference so we know what
-            // state we're evolving from.
-            let previousCellGrid = cellGrid.value.map((row) => [...row]);
+        function start() {
+            const loop = setInterval(() => {
+                // Copy the cell grid as a reference so we know what
+                // state we're evolving from.
+                let previousCellGrid = cellGrid.value.map((row) => [...row]);
 
-            // Modify the actual cellGrid.
-            cellGrid.value = cellGrid.value.map((row, x) => {
-                return row.map((alive, y) => {
-                    // Get all the neighbours.
-                    let neighbours = [
-                        previousCellGrid?.[x - 1]?.[y - 1] || false,
-                        previousCellGrid?.[x - 1]?.[y] || false,
-                        previousCellGrid?.[x - 1]?.[y + 1] || false,
+                // Modify the actual cellGrid.
+                cellGrid.value = cellGrid.value.map((row, x) => {
+                    return row.map((alive, y) => {
+                        // Get all the neighbours.
+                        let neighbours = [
+                            previousCellGrid?.[x - 1]?.[y - 1] || false,
+                            previousCellGrid?.[x - 1]?.[y] || false,
+                            previousCellGrid?.[x - 1]?.[y + 1] || false,
 
-                        previousCellGrid?.[x]?.[y - 1] || false,
-                        previousCellGrid?.[x]?.[y + 1] || false,
+                            previousCellGrid?.[x]?.[y - 1] || false,
+                            previousCellGrid?.[x]?.[y + 1] || false,
 
-                        previousCellGrid?.[x + 1]?.[y - 1] || false,
-                        previousCellGrid?.[x + 1]?.[y] || false,
-                        previousCellGrid?.[x + 1]?.[y + 1] || false,
-                    ];
+                            previousCellGrid?.[x + 1]?.[y - 1] || false,
+                            previousCellGrid?.[x + 1]?.[y] || false,
+                            previousCellGrid?.[x + 1]?.[y + 1] || false,
+                        ];
 
-                    let livingNeighbours = neighbours.filter(
-                        (alive) => alive === true
-                    ).length;
+                        let livingNeighbours = neighbours.filter(
+                            (alive) => alive === true
+                        ).length;
 
-                    return livingNeighbours > 1 && livingNeighbours <= 3;
+                        if (alive) {
+                            return (
+                                livingNeighbours > 1 && livingNeighbours <= 3
+                            );
+                        }
+
+                        return livingNeighbours === 3;
+                    });
                 });
-            });
-        }, 250);
+            }, 150);
+        }
 
-        return { cellGrid };
+        function onClick(col: number, row: number) {
+            cellGrid.value[row][col] = !cellGrid.value[row][col];
+        }
+
+        return { cellGrid, onClick, start };
     },
 });
 </script>
@@ -94,5 +109,11 @@ export default defineComponent({
 
 .Row {
     @apply flex flex-row items-center;
+}
+
+.Start {
+    @apply absolute;
+    @apply top-0 right-0 m-1 px-4 py-0.5;
+    @apply border-2 rounded border-pink-400 bg-pink-200 text-pink-600;
 }
 </style>
