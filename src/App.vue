c605720ab8011512e8d3c6e68a2017bbc89e576c
Sara Rooijakkers <sara.rooijakkers@gmail.com>
2021-06-29 20:12:11 +0200

    Generate a grid of cells.

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,65 @@
 <template>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+    <div class="Row" v-for="(row, i) in cellGrid" :key="`row-${i}`">
+        <Cell
+            v-for="(alive, j) in row"
+            :key="`row-${i}-col-${j}`"
+            :alive="alive"
+        />
+    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent, ref } from "vue";
+import Cell from "./components/Cell.vue";
 
 export default defineComponent({
     name: "App",
-    components: {
-        HelloWorld,
+    components: { Cell },
+    setup() {
+        // A cell is a 20px cube.
+        const cellSize = 20;
+
+        // We use nested arrays to keep track of the cell state,
+        // with just a boolean to indicate if the cell is alive or not.
+        const cellGrid = ref<boolean[][]>([]);
+
+        // Make just enough cells to cover the screen.
+        function generateCellsForWindow() {
+            const cols = Math.floor(window.innerWidth / cellSize);
+            const rows = Math.floor(window.innerHeight / cellSize);
+
+            console.log(cols, rows);
+
+            // Clear the existing cell map.
+            cellGrid.value = [];
+
+            // Fill it with new cells.
+            for (let currentRow = 0; currentRow < rows; currentRow++) {
+                const row: boolean[] = [];
+                for (let currentCol = 0; currentCol < cols; currentCol++) {
+                    const alive = Math.random() > 0.8;
+                    row.push(alive);
+                }
+
+                cellGrid.value.push(row);
+            }
+        }
+
+        generateCellsForWindow();
+
+        return { cellGrid };
     },
 });
 </script>
 
 <style>
 #app {
-    @apply flex flex-col items-center;
-    @apply text-gray-600;
-    @apply mt-16;
+    @apply flex flex-col items-center justify-center;
+    @apply bg-gray-900;
+    @apply w-screen h-screen overflow-hidden;
+}
+
+.Row {
+    @apply flex flex-row items-center;
 }
 </style>
---
+++ src/components/Cell.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="Cell" :class="{ 'Cell--alive': alive }"></div>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from "vue";
+export default defineComponent({
+    name: "Cell",
+    props: {
+        alive: {
+            type: Boolean,
+            required: true,
+        },
+    },
+});
+</script>
+
+<style scoped>
+.Cell {
+    @apply w-4 h-4 border-2 rounded m-0.5;
+    @apply bg-pink-800 border-pink-600;
+    @apply opacity-0;
+    @apply transition-opacity;
+}
+
+.Cell--alive {
+    @apply opacity-100;
+}
+</style>
